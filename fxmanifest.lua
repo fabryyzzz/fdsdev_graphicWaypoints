b79dd0f99ad98f10c3a4bcaf9df1a497b248f03e
Fabrizio - FDS Development <58892804+fabryyzzz@users.noreply.github.com>
2024-01-14 01:12:25 +0100

    fxmanifest.lua File

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+
+-- https://discord.gg/amEJNfn9HX FOR SUPPORT
+
+fx_version 'adamant'
+games { 'gta5' }
+lua54 'yes'
+author 'FDS DEVELOPMENT - FiveM Resources'
+description 'graphic Waypoints - https://discord.gg/amEJNfn9HX'
+version '1.0.0'
+   
+client_scripts {
+	"client.lua",
+}
+
+shared_scripts {
+	"config.lua",
+}
+
+-- https://discord.gg/amEJNfn9HX
+-- https://discord.gg/amEJNfn9HX
+-- https://discord.gg/amEJNfn9HX
+-- https://discord.gg/amEJNfn9HX
+-- https://discord.gg/amEJNfn9HX
+-- https://discord.gg/amEJNfn9HX
+-- DISCORD FOR SUPPORT
