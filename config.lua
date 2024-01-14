24b98fcb26929f3cd5ea2b8477f3c01e6cc57b73
Fabrizio - FDS Development <58892804+fabryyzzz@users.noreply.github.com>
2024-01-14 01:11:54 +0100

    Configuration file

---
+++ config.lua
@@ -0,0 +1,15 @@
+-- FDS Development
+-- Please consider to join our discord discord.gg/amEJNfn9HX for other resources and discounts.
+-- Thank you very much! <3
+-- fdsdevelopment.tebex.io
+Config = {}
+Config.Button = "MOUSE_MIDDLE"
+Config.ButtonType = "MOUSE_BUTTON"
+
+
+Config.KeybindTitle = 'Set a waypoint'
+
+Config.MarkerSize = vec3(10.4, 10.0, 9000.0)
+Config.MarkerColor = {r = 232, g = 92, b = 250, a = 50}
+
+-- Made by FDS Development ---- fabryy on discord. 
